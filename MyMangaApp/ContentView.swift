d1684d04ff3b0b6300d092507e2e97cde72e104b
Daniel Murcia <daniel.steven.murcia@gmal.com>
2023-12-19 16:51:21 -0500

    Initial Commit

---
+++ MyMangaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMangaApp
+//
+//  Created by Alex Murcia on 19/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMangaApp/MyMangaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMangaAppApp.swift
+//  MyMangaApp
+//
+//  Created by Alex Murcia on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MyMangaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
